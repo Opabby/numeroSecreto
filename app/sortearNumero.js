65d6969ba9e466e419cd9651f00048e707fe8edd
Thaís Rodeiro <opabby82@gmail.com>
2024-05-19 19:54:35 -0300

    Refatorando para tornar maiorValor e menorValor variáveis globais

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,17 +1,15 @@
-const sorteio = gerarNumeroAleatorio();
+const maiorValor = 1000;
+const menorValor = 0;
+
+const elementoMenorValor = document.getElementById('menor-valor');
+elementoMenorValor.innerHTML = menorValor;
+
+const elementoMaiorValor = document.getElementById('maior-valor');
+elementoMaiorValor.innerHTML = maiorValor;
 
 function gerarNumeroAleatorio() {
-    const maiorValor = 1000;
-    const menorValor = 0;
-
-    const elementoMenorValor = document.getElementById('menor-valor');
-    elementoMenorValor.innerHTML = menorValor;
-
-    const elementoMaiorValor = document.getElementById('maior-valor');
-    elementoMaiorValor.innerHTML = maiorValor;
-
-    
-    return parseInt(Math.random() * ( (maiorValor+1) - menorValor ) + menorValor)
+      return parseInt(Math.random() * ( (maiorValor+1) - menorValor ) + menorValor)
 }
 
+const sorteio = gerarNumeroAleatorio();
 console.log('Número secreto: ', sorteio);
